7b728d9ba3c5a41e89e9617b150fb741aeb83f9e
Ana Sofia Alves <aslas@iscte-iul.pt>
2020-03-19 15:02:47 +0000

    Initial commit

---
+++ SID_Software_House/src/main/java/g30/SID_Software_House/App.java
@@ -0,0 +1,13 @@
+package g30.SID_Software_House;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
